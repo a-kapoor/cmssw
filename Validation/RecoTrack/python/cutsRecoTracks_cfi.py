28a3274c0fc65b13672449bfc4c8f78133958864
anshul <anshul.kapoor@students.iiserpune.ac.in>
2015-09-29 12:41:42 +0200

    Extracted The Actual InitialStepTrack Collection for validation, instead of using flagged InitialStepTracks in generakTrack Collection

--- Validation/RecoTrack/python/cutsRecoTracks_cfi.py
+++ Validation/RecoTrack/python/cutsRecoTracks_cfi.py
@@ -2,3 +2,4 @@ import FWCore.ParameterSet.Config as cms
 
 import CommonTools.RecoAlgos.recoTrackViewRefSelector_cfi
 cutsRecoTracks = CommonTools.RecoAlgos.recoTrackViewRefSelector_cfi.recoTrackViewRefSelector.clone()
+cutsInitialTracks = CommonTools.RecoAlgos.recoTrackViewRefSelector_cfi.recoTrackViewRefSelector2.clone()
